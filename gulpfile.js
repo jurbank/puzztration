7089edad73b61540cc3114f9683097a0ca2a7868
John Urbank <jurbank@gmail.com>
2014-03-26 22:10:18 -0400

    architecture restructure, added live-reload to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,39 @@
+
 var gulp = require('gulp');
-
-// package.json
-// var pkg = require('./package.json');
-
+var connect = require('gulp-connect');
 var sass = require('gulp-ruby-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');  
 
+gulp.task('connect', connect.server({
+  root: ['app'],
+  port: 4000,
+  livereload: true
+}));
+
+gulp.task('html', function () {
+  return gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
 
 gulp.task('css', function() {
-	return gulp.src('sass/app.scss')
-		.pipe(sass({style: 'compressed'}))
-		.pipe(autoprefixer('last 5 version'))
-		.pipe(gulp.dest('css'));
+  return gulp.src('./app/sass/*.scss')
+    .pipe(sass({style: 'compressed'}))
+    .pipe(autoprefixer('last 5 version'))
+    .pipe(gulp.dest('./app/css'))
+    .pipe(connect.reload());
 });
 
-
-// The default task (called when you run `gulp`)
-gulp.task('default', function() {
-  gulp.run('css');
-
-  gulp.watch('sass/**/*.scss', function() {
-  	gulp.run('css');
-  });
-
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/sass/*.scss'], ['css']);
 });
+
+gulp.task('default', ['connect', 'watch']);
+
+
+
+
+
+
 
 
